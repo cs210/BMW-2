d3681639fea32f07ff3ef05ef880d0dd2064088a
wjq8g6 <jwang98@stanford.edu>
2020-04-27 17:46:17 -0700

    added intro menu

--- dist-server/server/AsteroidsServerEngine.js
+++ dist-server/server/AsteroidsServerEngine.js
@@ -15,7 +15,13 @@ var _Ship = _interopRequireDefault(require("../common/Ship"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _createForOfIteratorHelper(o) { if (typeof Symbol === "undefined" || o[Symbol.iterator] == null) { if (Array.isArray(o) || (o = _unsupportedIterableToArray(o))) { var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var it, normalCompletion = true, didErr = false, err; return { s: function s() { it = o[Symbol.iterator](); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it["return"] != null) it["return"](); } finally { if (didErr) throw err; } } }; }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(n); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
@@ -23,31 +29,35 @@ function _defineProperties(target, props) { for (var i = 0; i < props.length; i+
 
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
-
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
 function _get(target, property, receiver) { if (typeof Reflect !== "undefined" && Reflect.get) { _get = Reflect.get; } else { _get = function _get(target, property, receiver) { var base = _superPropBase(target, property); if (!base) return; var desc = Object.getOwnPropertyDescriptor(base, property); if (desc.get) { return desc.get.call(receiver); } return desc.value; }; } return _get(target, property, receiver || target); }
 
 function _superPropBase(object, property) { while (!Object.prototype.hasOwnProperty.call(object, property)) { object = _getPrototypeOf(object); if (object === null) break; } return object; }
-
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
 
 function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
 
-var AsteroidsServerEngine =
-/*#__PURE__*/
-function (_ServerEngine) {
+function _createSuper(Derived) { return function () { var Super = _getPrototypeOf(Derived), result; if (_isNativeReflectConstruct()) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+var AsteroidsServerEngine = /*#__PURE__*/function (_ServerEngine) {
   _inherits(AsteroidsServerEngine, _ServerEngine);
+
+  var _super = _createSuper(AsteroidsServerEngine);
 
   function AsteroidsServerEngine(io, gameEngine, inputOptions) {
     var _this;
 
     _classCallCheck(this, AsteroidsServerEngine);
 
-    _this = _possibleConstructorReturn(this, _getPrototypeOf(AsteroidsServerEngine).call(this, io, gameEngine, inputOptions));
+    _this = _super.call(this, io, gameEngine, inputOptions);
     gameEngine.physicsEngine.world.on('beginContact', _this.handleCollision.bind(_assertThisInitialized(_this)));
     gameEngine.on('shoot', _this.shoot.bind(_assertThisInitialized(_this)));
     return _this;
@@ -131,30 +141,20 @@ function (_ServerEngine) {
     value: function onPlayerDisconnected(socketId, playerId) {
       _get(_getPrototypeOf(AsteroidsServerEngine.prototype), "onPlayerDisconnected", this).call(this, socketId, playerId);
 
-      var _iteratorNormalCompletion = true;
-      var _didIteratorError = false;
-      var _iteratorError = undefined;
+      var _iterator = _createForOfIteratorHelper(this.gameEngine.world.queryObjects({
+        playerId: playerId
+      })),
+          _step;
 
       try {
-        for (var _iterator = this.gameEngine.world.queryObjects({
-          playerId: playerId
-        })[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+        for (_iterator.s(); !(_step = _iterator.n()).done;) {
           var o = _step.value;
           this.gameEngine.removeObjectFromWorld(o.id);
         }
       } catch (err) {
-        _didIteratorError = true;
-        _iteratorError = err;
+        _iterator.e(err);
       } finally {
-        try {
-          if (!_iteratorNormalCompletion && _iterator["return"] != null) {
-            _iterator["return"]();
-          }
-        } finally {
-          if (_didIteratorError) {
-            throw _iteratorError;
-          }
-        }
+        _iterator.f();
       }
     }
   }]);
--- src/client/clientEntryPoint.js
+++ src/client/clientEntryPoint.js
@@ -3,6 +3,7 @@ import { Lib } from 'lance-gg';
 import AsteroidsClientEngine from '../client/AsteroidsClientEngine';
 import AsteroidsGameEngine from '../common/AsteroidsGameEngine';
 const qsOptions = querystring.parse(location.search);
+const $ = require('jquery');
 
 // default options, overwritten by query-string options
 // is sent to both game engine and client engine
@@ -20,7 +21,33 @@ const defaults = {
 let options = Object.assign(defaults, qsOptions);
 
 // create a client engine and a game engine
-const gameEngine = new AsteroidsGameEngine(options);
-const clientEngine = new AsteroidsClientEngine(gameEngine, options);
+// const gameEngine = new AsteroidsGameEngine(options);
+// const clientEngine = new AsteroidsClientEngine(gameEngine, options);
+// document.addEventListener('DOMContentLoaded', function(e) { clientEngine.start(); });
 
-document.addEventListener('DOMContentLoaded', function(e) { clientEngine.start(); });
+$(document).ready(() => {
+    $('#name-input').focus();
+    const sendName = () => {
+        const name = $('#name-input').val();
+        let gamecode = $('#gamecode-input').val();
+
+        if (gamecode.length === 0) {
+            gamecode = '/lobby';
+        }
+        if (name && name.length < 20) {
+            options.playerOptions = {
+                playerName: name,
+                roomName: gamecode
+            };
+            const gameEngine = new AsteroidsGameEngine(options);
+            const clientEngine = new AsteroidsClientEngine(gameEngine, options);
+            $('#name-prompt-overlay').remove();
+            clientEngine.start();
+        } else {
+            window.alert('Your name cannot be blank or over 20 characters.');
+        }
+        return false;
+    };
+    $('#name-form').submit(sendName);
+    $('#name-submit').click(sendName);
+});
