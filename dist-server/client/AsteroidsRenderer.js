5ea9445a96581a7a503655503acd419d0e5a9f5d
Ricky Singh <Eric@Erics-MacBook-Pro-6.local>
2020-05-16 15:43:41 -0700

    Added feedback button

---
+++ dist-server/client/AsteroidsRenderer.js
@@ -0,0 +1,246 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _lanceGg = require("lance-gg");
+
+var _Asteroid = _interopRequireDefault(require("./../common/Asteroid"));
+
+var _Bullet = _interopRequireDefault(require("./../common/Bullet"));
+
+var _Ship = _interopRequireDefault(require("./../common/Ship"));
+
+var _FinishLine = _interopRequireDefault(require("../common/FinishLine"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _get(target, property, receiver) { if (typeof Reflect !== "undefined" && Reflect.get) { _get = Reflect.get; } else { _get = function _get(target, property, receiver) { var base = _superPropBase(target, property); if (!base) return; var desc = Object.getOwnPropertyDescriptor(base, property); if (desc.get) { return desc.get.call(receiver); } return desc.value; }; } return _get(target, property, receiver || target); }
+
+function _superPropBase(object, property) { while (!Object.prototype.hasOwnProperty.call(object, property)) { object = _getPrototypeOf(object); if (object === null) break; } return object; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _createSuper(Derived) { return function () { var Super = _getPrototypeOf(Derived), result; if (_isNativeReflectConstruct()) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+var ctx = null;
+var game = null;
+var canvas = null;
+
+var AsteroidsRenderer = /*#__PURE__*/function (_Renderer) {
+  _inherits(AsteroidsRenderer, _Renderer);
+
+  var _super = _createSuper(AsteroidsRenderer);
+
+  function AsteroidsRenderer(gameEngine, clientEngine) {
+    var _this;
+
+    _classCallCheck(this, AsteroidsRenderer);
+
+    _this = _super.call(this, gameEngine, clientEngine);
+    game = gameEngine; // Init canvas
+
+    canvas = document.createElement('canvas');
+    canvas.style.visibility = 'hidden';
+    canvas.width = window.innerWidth * window.devicePixelRatio;
+    canvas.height = window.innerHeight * window.devicePixelRatio;
+    document.body.insertBefore(canvas, document.getElementById('logo'));
+    game.w = canvas.width;
+    game.h = canvas.height;
+    game.zoom = game.h / game.spaceHeight;
+    if (game.w / game.spaceWidth < game.zoom) game.zoom = game.w / game.spaceWidth;
+    game.zoom = game.h / game.spaceHeight;
+    ctx = canvas.getContext('2d');
+    ctx.lineWidth = 2 / game.zoom;
+    ctx.strokeStyle = ctx.fillStyle = 'white';
+    ctx.shadowBlur = 10;
+    ctx.shadowColor = "white";
+    ctx.font = "0.2px ONEDAY";
+    ctx.textAlign = "center";
+    _this.viewer = false;
+    _this.groupShipPID = null;
+    return _this;
+  }
+
+  _createClass(AsteroidsRenderer, [{
+    key: "showCanvas",
+    value: function showCanvas() {
+      canvas.style.visibility = 'visible';
+    }
+  }, {
+    key: "draw",
+    value: function draw(t, dt) {
+      var _this2 = this;
+
+      _get(_getPrototypeOf(AsteroidsRenderer.prototype), "draw", this).call(this, t, dt); // Clear the canvas
+
+
+      ctx.clearRect(0, 0, game.w, game.h); // Transform the canvas
+      // Note that we need to flip the y axis since Canvas pixel coordinates
+      // goes from top to bottom, while physics does the opposite.
+
+      ctx.save();
+      ctx.translate(game.w / 2, game.h / 2); // Translate to the center
+      // ctx.scale(game.zoom, -game.zoom);  // Zoom in and flip y axis
+
+      ctx.scale(game.zoom, game.zoom); // original y flip doesnt allow for text
+      // Draw all things
+      //this.drawBounds();
+
+      game.world.forEachObject(function (id, obj) {
+        if (obj instanceof _Ship["default"]) _this2.drawShip(obj.physicsObj, obj.playerId === _this2.groupShipPID, obj.c_name, obj.v_name);else if (obj instanceof _Bullet["default"]) _this2.drawBullet(obj.physicsObj);else if (obj instanceof _FinishLine["default"]) _this2.drawFinishLine(obj.physicsObj);else if (obj instanceof _Asteroid["default"] && _this2.viewer) _this2.drawAsteroid(obj.physicsObj);
+      }); // update status and restore
+
+      this.updateStatus();
+      ctx.restore();
+    }
+  }, {
+    key: "updateStatus",
+    value: function updateStatus() {
+      var playerShip = this.gameEngine.world.queryObject({
+        playerId: this.groupShipPID
+      });
+
+      if (!playerShip) {
+        /*if (this.lives == undefined)
+            document.getElementById('gameover').classList.remove('hidden');*/
+        return;
+      } // update score if necessary
+
+
+      if (playerShip.playerId === this.groupShipPID && this.score != playerShip.score) {
+        document.getElementById('score').innerHTML = 'Score: ' + playerShip.score;
+        this.score = playerShip.score;
+      } // update winning if necessary
+
+
+      if (playerShip.playerId === this.groupShipPID && playerShip.won) {//document.getElementById('gamewin').classList.remove('hidden');
+        // this.lives++;
+      }
+    }
+  }, {
+    key: "removeInstructions",
+    value: function removeInstructions() {
+      document.getElementById('instructions').classList.add('hidden');
+      document.getElementById('instructionsMobile').classList.add('hidden');
+    }
+  }, {
+    key: "drawShip",
+    value: function drawShip(body, special, c_name, v_name) {
+      var radius = body.shapes[0].radius;
+
+      if (special) {
+        ctx.strokeStyle = ctx.fillStyle = "#18CAE6";
+        ctx.shadowColor = "#18CAE6";
+      }
+
+      ctx.save();
+      ctx.translate(body.position[0], body.position[1]); // Translate to the ship center
+
+      ctx.fillText(v_name, 0, -0.63);
+      ctx.fillText(c_name, 0, -0.4);
+      ctx.rotate(body.angle); // Rotate to ship orientation
+
+      ctx.beginPath();
+
+      for (var j = 0; j < 3; j++) {
+        var xv = body.shapes[0].vertices[j][0];
+        var yv = body.shapes[0].vertices[j][1];
+        if (j == 0) ctx.moveTo(xv, yv);else ctx.lineTo(xv, yv);
+      }
+
+      ctx.closePath();
+      ctx.stroke();
+      ctx.restore();
+      ctx.strokeStyle = ctx.fillStyle = 'white';
+      ctx.shadowColor = "white";
+    }
+  }, {
+    key: "drawFinishLine",
+    value: function drawFinishLine(body) {
+      ctx.strokeStyle = ctx.fillStyle = "#FAF602";
+      ctx.shadowColor = "#FAF602";
+      ctx.save();
+      ctx.translate(body.position[0], body.position[1]); // Translate to the center
+
+      ctx.fillText("Finish", 0, 0); //ctx.rotate(.785);
+
+      ctx.beginPath();
+
+      for (var j = 0; j < game.numAsteroidVerts; j++) {
+        var xv = body.verts[j][0];
+        var yv = body.verts[j][1];
+        if (j == 0) ctx.moveTo(xv, yv);else ctx.lineTo(xv, yv);
+      }
+
+      ctx.closePath();
+      ctx.stroke();
+      ctx.restore();
+      ctx.strokeStyle = ctx.fillStyle = 'white';
+      ctx.shadowColor = "white";
+    }
+  }, {
+    key: "drawAsteroid",
+    value: function drawAsteroid(body) {
+      ctx.save();
+      ctx.translate(body.position[0], body.position[1]); // Translate to the center
+
+      ctx.beginPath();
+
+      for (var j = 0; j < game.numAsteroidVerts; j++) {
+        var xv = body.verts[j][0];
+        var yv = body.verts[j][1];
+        if (j == 0) ctx.moveTo(xv, yv);else ctx.lineTo(xv, yv);
+      }
+
+      ctx.closePath();
+      ctx.stroke();
+      ctx.restore();
+    }
+  }, {
+    key: "drawBullet",
+    value: function drawBullet(body) {
+      ctx.beginPath();
+      ctx.arc(body.position[0], body.position[1], game.bulletRadius, 0, 2 * Math.PI);
+      ctx.fill();
+      ctx.closePath();
+    }
+  }, {
+    key: "drawBounds",
+    value: function drawBounds() {
+      ctx.beginPath();
+      ctx.moveTo(-game.spaceWidth / 2, -game.spaceHeight / 2);
+      ctx.lineTo(-game.spaceWidth / 2, game.spaceHeight / 2);
+      ctx.lineTo(game.spaceWidth / 2, game.spaceHeight / 2);
+      ctx.lineTo(game.spaceWidth / 2, -game.spaceHeight / 2);
+      ctx.lineTo(-game.spaceWidth / 2, -game.spaceHeight / 2);
+      ctx.closePath();
+      ctx.stroke();
+    }
+  }]);
+
+  return AsteroidsRenderer;
+}(_lanceGg.Renderer);
+
+exports["default"] = AsteroidsRenderer;
+//# sourceMappingURL=AsteroidsRenderer.js.map
---
+++ dist-server/common/AsteroidsGameEngine.js
@@ -0,0 +1,445 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _lanceGg = require("lance-gg");
+
+var _Asteroid = _interopRequireDefault(require("./Asteroid"));
+
+var _Bullet = _interopRequireDefault(require("./Bullet"));
+
+var _Ship = _interopRequireDefault(require("./Ship"));
+
+var _FinishLine = _interopRequireDefault(require("./FinishLine"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _createForOfIteratorHelper(o) { if (typeof Symbol === "undefined" || o[Symbol.iterator] == null) { if (Array.isArray(o) || (o = _unsupportedIterableToArray(o))) { var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var it, normalCompletion = true, didErr = false, err; return { s: function s() { it = o[Symbol.iterator](); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it["return"] != null) it["return"](); } finally { if (didErr) throw err; } } }; }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(n); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _get(target, property, receiver) { if (typeof Reflect !== "undefined" && Reflect.get) { _get = Reflect.get; } else { _get = function _get(target, property, receiver) { var base = _superPropBase(target, property); if (!base) return; var desc = Object.getOwnPropertyDescriptor(base, property); if (desc.get) { return desc.get.call(receiver); } return desc.value; }; } return _get(target, property, receiver || target); }
+
+function _superPropBase(object, property) { while (!Object.prototype.hasOwnProperty.call(object, property)) { object = _getPrototypeOf(object); if (object === null) break; } return object; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _createSuper(Derived) { return function () { var Super = _getPrototypeOf(Derived), result; if (_isNativeReflectConstruct()) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+var AsteroidsGameEngine = /*#__PURE__*/function (_GameEngine) {
+  _inherits(AsteroidsGameEngine, _GameEngine);
+
+  var _super = _createSuper(AsteroidsGameEngine);
+
+  function AsteroidsGameEngine(options) {
+    var _this;
+
+    _classCallCheck(this, AsteroidsGameEngine);
+
+    _this = _super.call(this, options); // create physics with no friction; wrap positions after each step
+
+    _this.physicsEngine = new _lanceGg.P2PhysicsEngine({
+      gameEngine: _assertThisInitialized(_this)
+    });
+    _this.physicsEngine.world.defaultContactMaterial.friction = 10;
+
+    _this.on('postStep', _this.warpAll.bind(_assertThisInitialized(_this))); // game variables
+
+
+    Object.assign(_assertThisInitialized(_this), {
+      lives: 0,
+      shipSize: 0.4,
+      shipTurnSpeed: 0.035,
+      shipSpeed: 4,
+      bulletRadius: 0.03,
+      bulletLifeTime: 60,
+      asteroidRadius: 1.125,
+      numAsteroidLevels: 4,
+      numAsteroidVerts: 4,
+      maxAsteroidSpeed: 0,
+      spaceWidth: 16,
+      spaceHeight: 9,
+      SHIP: Math.pow(2, 1),
+      BULLET: Math.pow(2, 2),
+      ASTEROID: Math.pow(2, 3),
+      FINISHLINE: Math.pow(2, 4)
+    });
+    _this.playerReady = {};
+    return _this;
+  } // If the body is out of space bounds, warp it to the other side
+
+
+  _createClass(AsteroidsGameEngine, [{
+    key: "warpAll",
+    value: function warpAll() {
+      var _this2 = this;
+
+      this.world.forEachObject(function (id, obj) {
+        var p = obj.position;
+        var v = obj.velocity;
+
+        if (p.x > _this2.spaceWidth / 2) {
+          p.x = _this2.spaceWidth / 2;
+          v.x = 0;
+        }
+
+        if (p.y > _this2.spaceHeight / 2) {
+          p.y = _this2.spaceHeight / 2;
+          v.y = 0;
+        }
+
+        if (p.x < -_this2.spaceWidth / 2) {
+          p.x = -_this2.spaceWidth / 2;
+          v.x = 0;
+        }
+
+        if (p.y < -_this2.spaceHeight / 2) {
+          p.y = -_this2.spaceHeight / 2;
+          v.y = 0;
+        }
+
+        obj.refreshToPhysics();
+      });
+    }
+  }, {
+    key: "registerClasses",
+    value: function registerClasses(serializer) {
+      serializer.registerClass(_Ship["default"]);
+      serializer.registerClass(_Asteroid["default"]);
+      serializer.registerClass(_Bullet["default"]);
+      serializer.registerClass(_FinishLine["default"]);
+    }
+  }, {
+    key: "processInput",
+    value: function processInput(inputData, playerId) {
+      _get(_getPrototypeOf(AsteroidsGameEngine.prototype), "processInput", this).call(this, inputData, playerId);
+
+      if (playerId in this.playerReady && this.playerReady[playerId]) {
+        // handle keyboard presses
+        var playerShip = this.world.queryObject({
+          playerId: playerId,
+          instanceType: _Ship["default"]
+        });
+
+        if (playerShip) {
+          if (inputData.input === 'up') {
+            playerShip.physicsObj.applyForceLocal([0, -this.shipSpeed]);
+          } else if (inputData.input === 'right') {
+            playerShip.physicsObj.angle += this.shipTurnSpeed;
+          } else if (inputData.input === 'left') {
+            playerShip.physicsObj.angle -= this.shipTurnSpeed;
+          } else if (inputData.input === 'down') {
+            playerShip.physicsObj.applyForceLocal([0, this.shipSpeed]);
+          } else if (inputData.input === 'space') {
+            this.emit('shoot', playerShip);
+          }
+
+          playerShip.refreshFromPhysics();
+        }
+      }
+    } // create ship
+
+  }, {
+    key: "addShip",
+    value: function addShip(playerId, c_name, v_name) {
+      var score = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : 0;
+      var s = new _Ship["default"](this, {}, {
+        playerId: playerId,
+        mass: 10,
+        angularVelocity: 0,
+        position: new _lanceGg.TwoVector(-6.4, 3.6),
+        velocity: new _lanceGg.TwoVector(0, 0)
+      });
+      s.score = score;
+      s.won = false;
+      s.c_name = c_name;
+      s.v_name = v_name;
+      this.addObjectToWorld(s);
+    }
+  }, {
+    key: "getRandInt",
+    value: function getRandInt(min, max) {
+      // Includes min and max.
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+  }, {
+    key: "resetAllShips",
+    value: function resetAllShips() {
+      var _iterator = _createForOfIteratorHelper(this.world.queryObjects({
+        instanceType: _Ship["default"]
+      })),
+          _step;
+
+      try {
+        for (_iterator.s(); !(_step = _iterator.n()).done;) {
+          var o = _step.value;
+          this.resetShip(o);
+        }
+      } catch (err) {
+        _iterator.e(err);
+      } finally {
+        _iterator.f();
+      }
+    }
+  }, {
+    key: "resetShip",
+    value: function resetShip(ship) {
+      var old_score = ship.score;
+      var c_name = ship.c_name;
+      var v_name = ship.v_name;
+      var old_pid = ship.playerId;
+      this.removeObjectFromWorld(ship.id);
+      this.addShip(old_pid, c_name, v_name, old_score);
+    } // asteroid explosion
+
+  }, {
+    key: "explode",
+    value: function explode(asteroid, bullet) {
+      // Remove asteroid and bullet
+      var asteroidBody = asteroid.physicsObj;
+      var level = asteroid.level;
+      var x = asteroidBody.position[0];
+      var y = asteroidBody.position[1];
+      var r = this.asteroidRadius * (this.numAsteroidLevels - level) / this.numAsteroidLevels;
+      this.removeObjectFromWorld(asteroid);
+      this.removeObjectFromWorld(bullet); // Add new sub-asteroids
+
+      /*
+      if (level < 3) {
+          let angleDisturb = Math.PI/2 * Math.random();
+          for (let i=0; i<4; i++) {
+              let angle = Math.PI/2 * i + angleDisturb;
+              let subAsteroid = new Asteroid(this, {}, {
+                  mass: 10,
+                  position: new TwoVector(x + r * Math.cos(angle), y + r * Math.sin(angle)),
+                  velocity: new TwoVector(0, 0)
+              });
+              subAsteroid.level = level + 1;
+              this.trace.info(() => `creating sub-asteroid with radius ${r}: ${subAsteroid.toString()}`);
+              this.addObjectToWorld(subAsteroid);
+          }
+      }
+      */
+    }
+  }, {
+    key: "buildBarrier",
+    value: function buildBarrier(posX, posY, sizeX, sizeY) {
+      var barrier = new _Asteroid["default"](this, {}, {
+        mass: 100000,
+        position: new _lanceGg.TwoVector(posX, posY),
+        velocity: new _lanceGg.TwoVector(0, 0),
+        angularVelocity: 0
+      }, new _lanceGg.TwoVector(sizeX, sizeY));
+      barrier.level = 0;
+      this.addObjectToWorld(barrier);
+    } // Add finishline
+
+  }, {
+    key: "addFinishLine",
+    value: function addFinishLine(posX, posY) {
+      var a = new _FinishLine["default"](this, {}, {
+        mass: 10000,
+        position: new _lanceGg.TwoVector(posX, posY),
+        velocity: new _lanceGg.TwoVector(0, 0),
+        angularVelocity: 0
+      }, new _lanceGg.TwoVector(1, 1));
+      a.level = 0;
+      this.addObjectToWorld(a);
+    }
+  }, {
+    key: "removeAllBarriers",
+    value: function removeAllBarriers() {
+      var _iterator2 = _createForOfIteratorHelper(this.world.queryObjects({
+        instanceType: _Asteroid["default"]
+      })),
+          _step2;
+
+      try {
+        for (_iterator2.s(); !(_step2 = _iterator2.n()).done;) {
+          var o = _step2.value;
+          this.removeObjectFromWorld(o.id);
+        }
+      } catch (err) {
+        _iterator2.e(err);
+      } finally {
+        _iterator2.f();
+      }
+
+      var _iterator3 = _createForOfIteratorHelper(this.world.queryObjects({
+        instanceType: _FinishLine["default"]
+      })),
+          _step3;
+
+      try {
+        for (_iterator3.s(); !(_step3 = _iterator3.n()).done;) {
+          var _o = _step3.value;
+          this.removeObjectFromWorld(_o.id);
+        }
+      } catch (err) {
+        _iterator3.e(err);
+      } finally {
+        _iterator3.f();
+      }
+    }
+    /*
+     * World Generation Code
+     * 10 different levels
+     */
+
+  }, {
+    key: "addBarriers",
+    value: function addBarriers(currentWorld) {
+      var world_choice = currentWorld;
+
+      while (!world_choice || world_choice === currentWorld) {
+        world_choice = this.getRandInt(0, 7);
+      }
+
+      switch (world_choice) {
+        case 0:
+          this.empty_world();
+          break;
+
+        case 1:
+          this.one_block_world();
+          break;
+
+        case 2:
+          this.over_middle_under_world();
+          break;
+
+        case 3:
+          this.s_world();
+          break;
+
+        case 4:
+          this.enter_the_room();
+          break;
+
+        case 5:
+          this.crossroads();
+          break;
+
+        case 6:
+          this.s_tunnel();
+          break;
+
+        case 7:
+          this.spiral();
+          break;
+      }
+
+      return world_choice;
+    }
+  }, {
+    key: "empty_world",
+    value: function empty_world() {
+      this.addFinishLine(6.5, -3.75);
+    }
+  }, {
+    key: "one_block_world",
+    value: function one_block_world() {
+      this.buildBarrier(-5, 1.5, 1, 3);
+      this.addFinishLine(6.5, -3.75);
+    }
+  }, {
+    key: "over_middle_under_world",
+    value: function over_middle_under_world() {
+      this.buildBarrier(-5, 1.5, 1, 7);
+      this.buildBarrier(5, -1.5, 1, 7);
+      this.buildBarrier(0, -3, 1, 4);
+      this.buildBarrier(0, 3, 1, 4);
+      this.addFinishLine(6.5, -3.75);
+    }
+  }, {
+    key: "s_world",
+    value: function s_world() {
+      this.buildBarrier(-1.5, 2, 13, 1);
+      this.buildBarrier(1.5, -2, 13, 1);
+      this.addFinishLine(6.5, -3.75);
+    }
+  }, {
+    key: "enter_the_room",
+    value: function enter_the_room() {
+      this.buildBarrier(-1.5, 2, 13, 1);
+      this.buildBarrier(0, -3, 1, 4);
+      this.addFinishLine(-6.5, -3.75);
+    }
+  }, {
+    key: "crossroads",
+    value: function crossroads() {
+      this.buildBarrier(5, 3, 1, 1);
+      this.buildBarrier(5, 0, 1, 1);
+      this.addFinishLine(5, -3);
+      this.buildBarrier(2.5, 3, 1, 1);
+      this.buildBarrier(2.5, 0, 1, 1);
+      this.buildBarrier(2.5, -3, 1, 1);
+      this.buildBarrier(0, 3, 1, 1);
+      this.buildBarrier(0, 0, 1, 1);
+      this.buildBarrier(0, -3, 1, 1);
+      this.buildBarrier(-2.5, 3, 1, 1);
+      this.buildBarrier(-2.5, 0, 1, 1);
+      this.buildBarrier(-2.5, -3, 1, 1);
+      this.buildBarrier(-5, 3, 1, 1);
+      this.buildBarrier(-5, 0, 1, 1);
+      this.buildBarrier(-5, -3, 1, 1);
+    }
+  }, {
+    key: "s_tunnel",
+    value: function s_tunnel() {
+      // Top and Bottom
+      this.buildBarrier(0, 5, 11, 1);
+      this.buildBarrier(0, -5, 11, 1); // Left and Right
+
+      this.buildBarrier(5, -1, 1, 6);
+      this.buildBarrier(-5, 1, 1, 6); // Middle
+
+      this.buildBarrier(-1, -1.5, 7, 1);
+      this.buildBarrier(1, 1.5, 7, 1);
+      this.addFinishLine(6.5, -3.75);
+    }
+  }, {
+    key: "spiral",
+    value: function spiral() {
+      // Top and Bottom
+      this.buildBarrier(0, 7, 11, 1);
+      this.buildBarrier(0, -7, 11, 1); // Left and Right
+
+      this.buildBarrier(6, 0, 1, 13);
+      this.buildBarrier(-5, 1, 1, 6);
+      this.buildBarrier(2, 0.5, 1, 3); // Middle
+
+      this.buildBarrier(-1, -1.5, 7, 1);
+      this.buildBarrier(0, 1.5, 5, 1);
+      this.addFinishLine(0, 0);
+    }
+  }]);
+
+  return AsteroidsGameEngine;
+}(_lanceGg.GameEngine);
+
+exports["default"] = AsteroidsGameEngine;
+//# sourceMappingURL=AsteroidsGameEngine.js.map
